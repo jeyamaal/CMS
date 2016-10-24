22a81169598fadfb4c3819480768e912af91b527
Jathevan <jathevan15@gmail.com>
2016-10-24 23:37:31 +0530

    Revert "Jathevan"

--- AccessControlManagement/Models/Post.cs
+++ AccessControlManagement/Models/Post.cs
@@ -18,10 +18,10 @@ namespace AccessControlManagement.Models
         public System.DateTime post_date { get; set; }
         public int user_id { get; set; }
         public Nullable<int> category_id { get; set; }
+        public string picture { get; set; }
         public string post_description { get; set; }
         public string activity_log { get; set; }
         public string title { get; set; }
-        public byte[] image { get; set; }
     
         public virtual Advertisement Advertisement { get; set; }
         public virtual Article Article { get; set; }
