502f137c9e50dc2600f1bafb3b6c74315215a4e2
Umatharsini <umamuruges2994@gmail.com>
2016-10-24 06:15:32 +0530

    Add style sheet for Post Dashboard

---
+++ AccessControlManagement/Scripts/style.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(function () {
+
+    $(".nav-links").each(function (index) {
+
+        $(this).click(function () {
+
+            if (index != 4) {
+                $("#home-link").css('color', '');
+                $("#home-link").css('background-color', '');
+
+                $(".nav-clicked").addClass("effect-1");
+                $(".nav-clicked").addClass("effect-2");
+                $(".nav-clicked").removeClass("nav-clicked");
+
+                $(this).removeClass("effect-1");
+                $(this).removeClass("effect-2");
+                $(this).addClass("nav-clicked");
+
+                $(".nav-links").css("pointer-events", "none");
+
+                (function (index) {
+                    $("#header").fadeTo("1000", 0, function () { });
+                    $("#load-view").fadeTo("1000", 0, function () {
+                        $("#loading-place").addClass("still-loading");
+
+                        if (index == 0) {
+
+
+                        } else if (index == 1) {
+                                
+                        } else if (index == 2) {
+
+                        } else if (index == 3) {
+
+                        }
+                    });
+
+                })(index);
+            }
+        });
+    });
+
+
+});
+
