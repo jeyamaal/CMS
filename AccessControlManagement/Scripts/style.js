8a2a51cb1bc40fa0c48ebcd8c240393a09beeb1a
jeyamaal <marukan@gmail.com>
2016-10-27 05:36:12 +0530

    #Add File
    [style.css]
    [style.js]

--- AccessControlManagement/Scripts/style.js
+++ AccessControlManagement/Scripts/style.js
@@ -4,9 +4,9 @@
 
         $(this).click(function () {
 
-            if (index != 4) {
-                $("#dashboard-link").css('color', '');
-                $("#dashboard-link").css('background-color', '');
+            if (index != 3) {
+                $("#home-link").css('color', '');
+                $("#home-link").css('background-color', '');
 
                 $(".nav-clicked").addClass("effect-1");
                 $(".nav-clicked").addClass("effect-2");
@@ -21,16 +21,44 @@
                 (function (index) {
                     $("#header").fadeTo("1000", 0, function () { });
                     $("#load-view").fadeTo("1000", 0, function () {
-                        $("#loading-place").addClass("still-loading");
 
-                        if (index == 0) {
+                        if (index == 0)
+                        {
+                            $("#load-view").load("/Categories/Index", function () {
+                                //CategorySaveChanges();
+                                //$(".add-new-btn").removeAttr("id");
+                                //$(".add-new-btn").attr("data-target", "#add-modal-category");
 
+                                //$("#header").fadeTo("1000", 1, function () { });
+                                //$("#load-view").fadeTo("1000", 1, function () {
+                                //    $(".nav-links").css("pointer-events", "");
+                                //});
+                            }).error(function (a) {
 
-                        } else if (index == 1) {
-                                ;
-                        } else if (index == 2) {
+                            });
+                        }
+                        else if (index == 1)
+                        {
+                                
+                        }
+                        else if (index == 2)
+                        {
+                            //Settings
+                            $("#load-view").load("/Categories/_Setting", {partial: true }, function () {
+                                CategorySaveChanges();
+                                $(".add-new-btn").removeAttr("id");
+                                $(".add-new-btn").attr("data-target", "#add-modal-category");
 
-                        } else if (index == 3) {
+                                $("#header").fadeTo("1000", 1, function () { });
+                                $("#load-view").fadeTo("1000", 1, function () {
+                                    $(".nav-links").css("pointer-events", "");
+                                });
+                            }).error(function (a) {
+
+                            });
+                        }
+                        else
+                        {
 
                         }
                     });
