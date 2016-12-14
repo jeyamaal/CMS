d15f8aa0b2401f4b77b44609f16eb6230def36da
Shangavi <shangavisubendrarajah@gmail.com>
2016-12-14 11:48:28 +0530

    Add Category changes for style

---
+++ AccessControlManagement/Models/CategoryPost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AccessControlManagement.Models
+{
+    public class CategoryPost
+    {
+        public string categoryName { set; get; }
+        public string date { set; get; }
+        public string title { set; get; }
+    }
+}
